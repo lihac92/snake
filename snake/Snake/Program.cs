29d633fe99be308ea43fcaa1080e814667ae8ba3
Alex <azubik@yandex.ru>
2019-12-08 01:53:02 +0300

    Перемещение змейки!

--- snake/Snake/Program.cs
+++ snake/Snake/Program.cs
@@ -10,19 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-           
-            
-            Point1 p1 = new Point1(1, 3 , '*');
-            p1.Draw();
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
+            // Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
-            Point1 p2 = new Point1(4, 5, '#');
-            p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 4, '-');
-            line.Draw();
-
-            VerticalLine line1 = new VerticalLine(2, 6, 13, '+');
-            line1.Draw();
+            Point1 p = new Point1(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
             Console.ReadLine();
         }
---
+++ snake/Snake/Snake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake : Figure
+    {
+        public Snake(Point1 tail, int length, Direction direction)
+        {
+            pList = new List<Point1>();
+            for(int i = 0; i < length; i++)
+            {
+                Point1 p = new Point1(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+    }
+}
