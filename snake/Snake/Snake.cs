0a6cae550c494b3832911bf13be4a294b5030f41
Alex <azubik@yandex.ru>
2019-12-08 15:58:28 +0300

    Move Snake!

--- snake/Snake/Snake.cs
+++ snake/Snake/Snake.cs
@@ -8,8 +8,10 @@ namespace Snake
 {
     class Snake : Figure
     {
-        public Snake(Point1 tail, int length, Direction direction)
+        Direction direction;
+        public Snake(Point1 tail, int length, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point1>();
             for(int i = 0; i < length; i++)
             {
@@ -17,6 +19,24 @@ namespace Snake
                 p.Move(i, direction);
                 pList.Add(p);
             }
+        }
+
+        internal void Move()
+        {
+            Point1 tail = pList.First();
+            pList.Remove(tail);
+            Point1 head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+        public Point1 GetNextPoint()
+        {
+            Point1 head = pList.Last();
+            Point1 nextPoint = new Point1(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
         }
     }
 }
